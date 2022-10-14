30e8456d33496fab89e32b0f4f4ab20f0cf960c7
BLVD3 <00nvogel@gmail.com>
2022-10-14 18:08:28 +0200

    Added Hand and DemotionLogic + Comments

--- src/de/hhn/shogi/gamelogic/Hand.java
+++ src/de/hhn/shogi/gamelogic/Hand.java
@@ -1,13 +1,41 @@
 package de.hhn.shogi.gamelogic;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Hand {
+    // Represents all the Pieces this Hand holds
     private ArrayList<Piece> pieces = new ArrayList<Piece>();
 
+    // Add a piece to the Hand (and demote it)
     public void addPiece(Piece piece) {
+        piece.demote();
         pieces.add(piece);
     }
 
-    
+
+    public Piece removePiece(PieceType type) {
+        for (Piece piece : pieces) {
+            if (piece.getBaseType() == type) {
+                pieces.remove(piece);
+                return piece;
+            }
+        }
+        throw new IllegalArgumentException("Hand does not contain a piece of type: " + type);
+    }
+
+    public PieceType[] getAllPieces() {
+        if (pieces.size() == 0) {
+            return null;
+        }
+        PieceType[] returnValue = new PieceType[pieces.size()];
+        for (int i = 0; i < returnValue.length; i++) {
+            returnValue[i] = pieces.get(i).getBaseType();
+        }
+        return returnValue;
+    }
+
+    public int getHandSize() {
+        return pieces.size();
+    }
 }
--- src/de/hhn/shogi/gamelogic/Piece.java
+++ src/de/hhn/shogi/gamelogic/Piece.java
@@ -1,9 +1,27 @@
 package de.hhn.shogi.gamelogic;
 
 public class Piece {
+    // Current type important for gameLogic
     private PieceType type;
+    // Original type of the Piece. Important for frontend and handLogic
     private PieceType baseType;
+    // Is Piece in promoted state. Important for frontend
     private boolean promoted;
+
+    // Returns current Type for Logic
+    public PieceType getType() {
+        return type;
+    }
+
+    // Returns baseType used for UI and DemotionLogic
+    public PieceType getBaseType() {
+        return baseType;
+    }
+
+    // Returns true when the Piece is Promoted
+    public boolean isPromoted() {
+        return promoted;
+    }
 
     //Tries to promote the Piece. Returns whether the promotion was done successfully.
     public boolean promote() {
@@ -17,6 +35,13 @@ public class Piece {
         return true;
     }
 
+    // Demotes the Piece. Should not be called typically
+    public void demote() {
+        type = baseType;
+        promoted = false;
+    }
+
+    // Returns True when the Piece can Promote
     public boolean canPromote() {
         if (promoted) {
             return false;
