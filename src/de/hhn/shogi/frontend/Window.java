983e73bfa8dcb20c587ed2d9adc547dabee46300
HenriSta <henri@staudenrausch.de>
2022-10-14 23:28:25 +0200

    attempted to display the board

--- src/de/hhn/shogi/frontend/Window.java
+++ src/de/hhn/shogi/frontend/Window.java
@@ -5,25 +5,37 @@ import java.awt.*;
 
 public class Window extends JFrame {
     JLabel text = new JLabel();
-    int boardSize = 500;
+    JLabel boardImg = new JLabel();
+    int boardSize = 800;
 
     public Window() {
+        //window settings
         setTitle("Shogi");
         setSize(1000, 1000);
         setExtendedState(MAXIMIZED_BOTH);
         setLayout(null);
-        getContentPane().setBackground(new Color(226, 170, 102));
+        getContentPane().setBackground(new Color(92, 31, 31));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //test text
         text.setForeground(Color.WHITE);
         text.setFont(new Font("Arial Black", Font.PLAIN, 15));
         add(text);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //adds board
+        boardImg.setIcon(new ImageIcon("src/de/hhn/shogi/frontend/board.png"));
+        add(boardImg);
+        getContentPane().add(boardImg);
     }
 
     public void paint(Graphics g) {
         super.paint(g);
+        //updated every time the window is resized
+
         text.setBounds(10, 0, 500, 50);
         text.setText("width: " + getWidth() + ", height: " + getHeight());
+        boardImg.setBounds(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, boardSize, boardSize);
+
 
 
         g.drawLine(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, getWidth() / 2 + boardSize / 2, getHeight() / 2 - boardSize / 2);
