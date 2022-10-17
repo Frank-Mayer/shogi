2418524f87e154907d183a312795d249c319e1ca
HenriSta <henri@staudenrausch.de>
2022-10-18 01:06:18 +0200

    Shogi board anzeige

--- src/de/hhn/shogi/frontend/Window.java
+++ src/de/hhn/shogi/frontend/Window.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JFrame {
-//    JLabel text = new JLabel();
-    JLabel boardImg = new JLabel();
-    int boardSize = 800;
+    JLabel title = new JLabel();
+    JLabel hand = new JLabel();
+    int boardSize = 750;
 
     public Window() {
         //window settings
@@ -14,41 +14,30 @@ public class Window extends JFrame {
         setSize(1000, 1000);
         setExtendedState(MAXIMIZED_BOTH);
         setLayout(null);
-        getContentPane().setBackground(new Color(92, 31, 31));
+        getContentPane().setBackground(new Color(50, 40, 50));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //test text
-//        text.setForeground(Color.WHITE);
-//        text.setFont(new Font("Arial Black", Font.PLAIN, 15));
-//        getContentPane().add(text);
+        //title text
+        title.setText("Shogi");
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Arial Black", Font.PLAIN, 50));
+        title.setHorizontalAlignment(JLabel.CENTER);
+        getContentPane().add(title);
 
-        //adds board
-//        boardImg.setIcon(new ImageIcon("src/de/hhn/shogi/frontend/board.png"));
-//        add(boardImg);
-        getContentPane().add(boardImg);
+        //hand rect
+        hand.setBackground(new Color(55, 45, 55));
+        hand.setBounds(50 , 50, boardSize/2, boardSize);
+
+        getContentPane().add(hand);
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         //updated every time the window is resized
-//        text.setOpaque(false);
-//        text.setBackground(new Color(0, 0, 0, 0));
-//        text.setBounds(10, 0, 500, 500);
-//        text.setText("width: " + getWidth() + ", height: " + getHeight());
-        ImageIcon ic = new ImageIcon("src/de/hhn/shogi/frontend/board.png");
-        g.drawImage(ic.getImage(),(getWidth()/2)-(400),(getHeight()/2)-(400),ic.getImageObserver());
-        g.setColor(new Color(0xE4FFFFFF, true));
-        g.drawString("width: " + getWidth() + ", height: " + getHeight(),getWidth()/35, getHeight()/20);
-        g.setColor(new Color(0x000000,false));
-//        boardImg.setBounds(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, boardSize, boardSize);
+        title.setBounds(0, 0, getWidth(), 80);
 
 
-
-        g.drawLine(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, getWidth() / 2 + boardSize / 2, getHeight() / 2 - boardSize / 2);
-        g.drawLine(getWidth() / 2 - boardSize / 2, getHeight() / 2 + boardSize / 2, getWidth() / 2 + boardSize / 2, getHeight() / 2 + boardSize / 2);
-        g.drawLine(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, getWidth() / 2 - boardSize / 2, getHeight() / 2 + boardSize / 2);
-        g.drawLine(getWidth() / 2 + boardSize / 2, getHeight() / 2 - boardSize / 2, getWidth() / 2 + boardSize / 2, getHeight() / 2 + boardSize / 2);
-
-
+        ImageIcon ic = new ImageIcon("src/de/hhn/shogi/frontend/images/board.png");
+        g.drawImage(ic.getImage(), (getWidth() / 2) - (boardSize / 2), (getHeight() / 2) - (boardSize / 2), ic.getImageObserver());
     }
 }
