1f3494afe9d6e3ff54822b756fc8c75c2a8bc6ce
HenriSta <henri@staudenrausch.de>
2022-10-17 18:26:35 +0200

    validMove Methode -> Parameter zu Vec2 gemacht

--- src/de/hhn/shogi/frontend/Window.java
+++ src/de/hhn/shogi/frontend/Window.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Window extends JFrame {
     JLabel text = new JLabel();
-    JLabel boardImg = new JLabel();
+    JLabel boardImg;
     int boardSize = 800;
 
     public Window() {
@@ -20,11 +20,10 @@ public class Window extends JFrame {
         //test text
         text.setForeground(Color.WHITE);
         text.setFont(new Font("Arial Black", Font.PLAIN, 15));
-        add(text);
+        getContentPane().add(text);
 
         //adds board
-        boardImg.setIcon(new ImageIcon("src/de/hhn/shogi/frontend/board.png"));
-        add(boardImg);
+        boardImg = new JLabel(new ImageIcon("src/de/hhn/shogi/frontend/board.png"));
         getContentPane().add(boardImg);
     }
 
@@ -34,9 +33,8 @@ public class Window extends JFrame {
 
         text.setBounds(10, 0, 500, 50);
         text.setText("width: " + getWidth() + ", height: " + getHeight());
+
         boardImg.setBounds(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, boardSize, boardSize);
-
-
 
         g.drawLine(getWidth() / 2 - boardSize / 2, getHeight() / 2 - boardSize / 2, getWidth() / 2 + boardSize / 2, getHeight() / 2 - boardSize / 2);
         g.drawLine(getWidth() / 2 - boardSize / 2, getHeight() / 2 + boardSize / 2, getWidth() / 2 + boardSize / 2, getHeight() / 2 + boardSize / 2);
