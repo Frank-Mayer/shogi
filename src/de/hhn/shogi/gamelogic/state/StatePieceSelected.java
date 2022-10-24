0ef4af46b0911dce919c69593d80c0c6efcfb3e3
BLVD3 <00nvogel@gmail.com>
2022-10-24 18:52:22 +0200

    Updated StatePieceSelected.java

--- src/de/hhn/shogi/gamelogic/state/StatePieceSelected.java
+++ src/de/hhn/shogi/gamelogic/state/StatePieceSelected.java
@@ -31,12 +31,18 @@ public class StatePieceSelected extends PlayerTurnState {
                 currentPlayer.getHand().addPiece(beatPiece);
             }
             getMainWindow().removeIcons();
+            getMainWindow().setField(pos, board.getPiece(pos));
+            getMainWindow().setField(selectedPos, board.getPiece(selectedPos));
             ACTIVE_GAME.getState().changeState(new StateNeutralTurn(getSide() == BoardSide.SENTE ? BoardSide.GOTE : BoardSide.SENTE));
         } else if (board.occupied(pos)) {
             if (board.getPiece(pos).getSide() == getSide()) {
                 ACTIVE_GAME.getState().changeState(new StatePieceSelected(getSide(), pos));
+                getMainWindow().removeIcons();
                 getMainWindow().displayPossibleMoves(RuleLogic.getAllPossibleMoves(pos, board.getPiece(pos)));
             }
+        } else {
+            getMainWindow().removeIcons();
+            ACTIVE_GAME.getState().changeState(new StateNeutralTurn(getSide()));
         }
     }
 
